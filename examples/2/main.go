6c6ddbf4d0ef91677fd0c8fce64886afda29ec9f
Ricky <doraemonkey@qq.com>
2024-07-30 20:06:35 +0800

    chore: Add example code

---
+++ examples/2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/doraemonkeys/fast-mpmc"
+	"time"
+)
+
+func main() {
+	// Create a new Fast-MPMC queue with a minimum buffer capacity of 10
+	queue := mpmc.NewFastMpmc[int](10)
+
+	// Push elements to the queue
+	queue.Push(1, 2, 3)
+
+	// Pop all elements from the queue
+	elements := queue.WaitPopAll()
+	fmt.Println(*elements) // Output: [1 2 3]
+
+	// Recycle the buffer
+	queue.RecycleBuffer(elements)
+
+	// Push more elements to the queue
+	queue.Push(4, 5, 6)
+
+	// Pop all elements with a context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	elements, ok := queue.WaitPopAllContext(ctx)
+	if ok {
+		fmt.Println(*elements) // Output: [4 5 6]
+	} else {
+		fmt.Println("Failed to pop elements within the timeout")
+	}
+
+	// Recycle the buffer
+	queue.RecycleBuffer(elements)
+}
